683a1cfea2f798eff4cac0b52bc6e632640ee4e8
mvb2001 <mahimabashitha2001@gmail.com>
2025-09-20 15:24:29 +0530

    project initialization

---
+++ src/main/java/org/real/smart_task/SmartTaskApplication.java
@@ -0,0 +1,13 @@
+package org.real.smart_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTaskApplication.class, args);
+    }
+
+}
